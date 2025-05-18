1e309587be83f3fc59ee6fcaa9e652f53007c9ed
Hussein Yasser <hissan_front@apexmindcoders.com>
2025-05-19 00:38:51 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:g_hotle/widgets/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home:  Container(),
+      home:  HomePage(),
     );
   }
 }
